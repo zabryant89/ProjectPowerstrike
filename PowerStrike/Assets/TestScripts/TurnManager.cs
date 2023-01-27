8948db8a969af768ac02891698cb3793dd858a2c
zabryant89 <zachary.a.bryant1@gmail.com>
2023-01-26 20:18:48 -0500

    Just a Minor Change
    Noting some things in comments.

--- PowerStrike/Assets/TestScripts/TurnManager.cs
+++ PowerStrike/Assets/TestScripts/TurnManager.cs
@@ -7,6 +7,7 @@ public class TurnManager : MonoBehaviour
     //created this script to deal with managing turns for each object
     //will save me a lot of repeated code.  Most of this is copy/paste from Player/Enemy scripts
     private float turnInterval; //increment of turn timer
+            //I'm not sure if I even need the turnInterval variable in this script...
     private float nextTurn; //actual time to pause for entity's turn
     private bool scheduled; //marks if the next turn has been scheduled or not, important for "turn alteration" effects!
     private bool turn; //is it the entity's turn? (used to determine if actions can be taken or not)
